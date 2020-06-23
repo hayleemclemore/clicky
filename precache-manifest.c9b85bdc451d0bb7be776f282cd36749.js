881efeeba8e95e59636b76feedc7a2a6008a6bda
Haylee McLemore <hmmclemore@gmail.com>
2020-06-22 21:48:11 -0500

    Updates

---
+++ precache-manifest.c9b85bdc451d0bb7be776f282cd36749.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad0b38715e83eaa427acb1aec1e0b34",
+    "url": "/clicky-app/index.html"
+  },
+  {
+    "revision": "94bb7921d1bf96c9c469",
+    "url": "/clicky-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8aaa4435882e4d8b146",
+    "url": "/clicky-app/static/js/2.313dcac7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-app/static/js/2.313dcac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94bb7921d1bf96c9c469",
+    "url": "/clicky-app/static/js/main.6c9adc27.chunk.js"
+  },
+  {
+    "revision": "855c2731bac2b794e8c0",
+    "url": "/clicky-app/static/js/runtime-main.3e56f02d.js"
+  },
+  {
+    "revision": "b463fbf308acd5f70e8a4ede6591a46e",
+    "url": "/clicky-app/static/media/sunset-1.b463fbf3.jpg"
+  },
+  {
+    "revision": "9be3029611d7d9679eb0e487f23fc5ca",
+    "url": "/clicky-app/static/media/sunset-10.9be30296.jpg"
+  },
+  {
+    "revision": "1ebafe0a79cab7185718d132fc620389",
+    "url": "/clicky-app/static/media/sunset-2.1ebafe0a.jpg"
+  },
+  {
+    "revision": "3ae152a38144b4f2b39dde71c3bb67fd",
+    "url": "/clicky-app/static/media/sunset-3.3ae152a3.jpg"
+  },
+  {
+    "revision": "dba95e5dee070d63dc0f3f06bbc2d37f",
+    "url": "/clicky-app/static/media/sunset-4.dba95e5d.jpg"
+  },
+  {
+    "revision": "c9dfb7abba18b83f25f9d4626cf38322",
+    "url": "/clicky-app/static/media/sunset-5.c9dfb7ab.jpg"
+  },
+  {
+    "revision": "a208f1fc2b9fe677f4a1b85abee5039f",
+    "url": "/clicky-app/static/media/sunset-6.a208f1fc.jpg"
+  },
+  {
+    "revision": "ecb15c45422c63cba5456d5cbb3ab69a",
+    "url": "/clicky-app/static/media/sunset-7.ecb15c45.jpg"
+  },
+  {
+    "revision": "8feb95cfb0ad6f056ca11e91818fd72d",
+    "url": "/clicky-app/static/media/sunset-8.8feb95cf.jpg"
+  },
+  {
+    "revision": "b28f7badf36fb3fc17e9ec3cc31a34f6",
+    "url": "/clicky-app/static/media/sunset-9.b28f7bad.jpg"
+  }
+]);
